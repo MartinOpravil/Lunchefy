a1e671418f885098938ae92d26ae4a71f6b83da8
Martin Opravil <m.opravil@gmail.com>
2025-03-26 14:30:49 +0100

    attempt to fix redirection glitch manifesting when opening the app after some period of time

--- lib/authentication.ts
+++ lib/authentication.ts
@@ -3,16 +3,18 @@ import { redirect } from "next/navigation";
 
 export async function getAuthToken() {
   const { userId, getToken } = await auth();
-  if (!userId) {
-    console.log("User is logged out");
-    redirect("/sign-in?loggedOut=true");
-  }
 
   try {
     const token = await getToken({ template: "convex" });
-    return token ?? undefined;
+
+    if (!token) {
+      console.log("No token available, redirecting to sign-in...");
+      redirect("/sign-in?loggedOut=true");
+    }
+
+    return token;
   } catch (error) {
-    console.log("Error when trying to retrieve a token");
+    console.log("Error retrieving token:", error);
     redirect("/sign-in?loggedOut=true");
   }
 }
